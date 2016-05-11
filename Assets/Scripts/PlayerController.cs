457e34471eee0a0df12ec7577a3975bba9fa93bc
iams3cond <andr3whunter@gmail.com>
2016-05-10 23:23:08 -0400

    Joystick for mobile
    movable cube with boost button

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+	
+	Rigidbody2D myBody;
+	public float moveForce= 5, boostMultiplier = 10;
+
+	void Start ()
+	{
+		myBody = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update ()
+	{
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+		bool isAction = CrossPlatformInputManager.GetButton ("Action");
+
+
+		myBody.AddForce (moveVec * (isAction ? boostMultiplier : 1));
+
+	}
+}
